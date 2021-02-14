86fe26f00ee4faff1fb20607d1b9226edd48689b
Axrk <maks_zyrin4@mail.ru>
2021-02-14 16:31:42 +0300

    AAAAAAAAAAAAAAAAAA

--- OslikTheGame/OslikTheGame.cpp
+++ OslikTheGame/OslikTheGame.cpp
@@ -5,7 +5,10 @@ using namespace std;
 int main()
 {
     std::cout << "Hello World!\n";
+    cout << "Suka" << endl;
 }
 
+// govno
+// next izmenenie
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
 // Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
